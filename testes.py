944706dcbdb0fff3f6aa2f6bd00d396233a87a61
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-03-31 17:32:35 -0300

    adding more tests

--- testes.py
+++ testes.py
@@ -2,7 +2,104 @@ import unittest
 
 from datetime import date
 
-from DateObjects import Date, Day, Month, Year
+from DateObjects import Date, Day, Month, Year, DayDelta, MonthDelta, YearDelta
+
+
+class TestDayDeltaDate(unittest.TestCase):
+
+    def test_DayDelta_add_Date(self):
+        self.assertEqual(Date(1, 1, 10), DayDelta(9) + Date(1, 1, 1))
+
+    def test_Date_add_DayDelta(self):
+        self.assertEqual(Date(1, 1, 10), Date(1, 1, 1) + DayDelta(9))
+
+    def test_february_leap_year_27_add_3(self):
+        self.assertEqual(Date(1992, 3, 1), Date(1992, 2, 27) + DayDelta(3))
+
+    def test_february_not_leap_year_27_add_3(self):
+        self.assertEqual(Date(1991, 3, 2), Date(1991, 2, 27) + DayDelta(3))
+
+    def test_add_365_days(self):
+        self.assertEqual(Date(2, 1, 1), Date(1, 1, 1) + DayDelta(365))
+        self.assertEqual(Date(1993, 2, 28), Date(1992, 2, 29) + DayDelta(365))
+
+    def test_add_366_days_in_leap_year(self):
+        self.assertEqual(Date(2021, 1, 1), Date(2020, 1, 1) + DayDelta(366))
+        self.assertEqual(Date(1992, 2, 29), Date(1991, 2, 28) + DayDelta(366))
+
+    def remove_day_2020_3_1(self):
+        self.assertEqual(Date(2020, 2, 29), Date(2020, 3, 1) - DayDelta(1))
+        self.assertEqual(Date(2020, 2, 29), Date(2020, 3, 1) + DayDelta(-1))
+
+    def remove_day_2021_3_1(self):
+        self.assertEqual(Date(2021, 2, 28), Date(2021, 3, 1) - DayDelta(1))
+        self.assertEqual(Date(2021, 2, 28), Date(2021, 3, 1) + DayDelta(-1))
+
+
+class TestDayDeltaOperations(unittest.TestCase):
+    def test_eq(self):
+        self.assertEqual(DayDelta(1), DayDelta(1))
+
+    def test_ne(self):
+        self.assertNotEqual(DayDelta(1), DayDelta(2))
+
+    def test_lt(self):
+        self.assertLess(DayDelta(1), DayDelta(2))
+
+    def test_le(self):
+        self.assertLessEqual(DayDelta(1), DayDelta(2))
+        self.assertLessEqual(DayDelta(2), DayDelta(2))
+
+    def test_gt(self):
+        self.assertGreater(DayDelta(2), DayDelta(1))
+
+    def test_ge(self):
+        self.assertGreaterEqual(DayDelta(2), DayDelta(1))
+        self.assertGreaterEqual(DayDelta(2), DayDelta(2))
+
+
+class TestMonthDeltaOperations(unittest.TestCase):
+    def test_eq(self):
+        self.assertEqual(MonthDelta(1), MonthDelta(1))
+
+    def test_ne(self):
+        self.assertNotEqual(MonthDelta(1), MonthDelta(2))
+
+    def test_lt(self):
+        self.assertLess(MonthDelta(1), MonthDelta(2))
+
+    def test_le(self):
+        self.assertLessEqual(MonthDelta(1), MonthDelta(2))
+        self.assertLessEqual(MonthDelta(2), MonthDelta(2))
+
+    def test_gt(self):
+        self.assertGreater(MonthDelta(2), MonthDelta(1))
+
+    def test_ge(self):
+        self.assertGreaterEqual(MonthDelta(2), MonthDelta(1))
+        self.assertGreaterEqual(MonthDelta(2), MonthDelta(2))
+
+
+class TestYearDeltaOperations(unittest.TestCase):
+    def test_eq(self):
+        self.assertEqual(YearDelta(1), YearDelta(1))
+
+    def test_ne(self):
+        self.assertNotEqual(YearDelta(1), YearDelta(2))
+
+    def test_lt(self):
+        self.assertLess(YearDelta(1), YearDelta(2))
+
+    def test_le(self):
+        self.assertLessEqual(YearDelta(1), YearDelta(2))
+        self.assertLessEqual(YearDelta(2), YearDelta(2))
+
+    def test_gt(self):
+        self.assertGreater(YearDelta(2), YearDelta(1))
+
+    def test_ge(self):
+        self.assertGreaterEqual(YearDelta(2), YearDelta(1))
+        self.assertGreaterEqual(YearDelta(2), YearDelta(2))
 
 
 class TestDayOperations(unittest.TestCase):
